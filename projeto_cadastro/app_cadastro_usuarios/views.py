b768c5ed790348e70bc69cd09b9368e7c7c9f92c
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-03-09 21:35:55 -0300

    :sparkles: Finalizando css da página de usuários

--- projeto_cadastro/app_cadastro_usuarios/views.py
+++ projeto_cadastro/app_cadastro_usuarios/views.py
@@ -9,3 +9,9 @@ def usuarios(request):
     novo_usuario.nome = request.POST.get('nome')
     novo_usuario.idade = request.POST.get('idade')
     novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    return render(request, 'usuarios/usuarios.html', usuarios)
